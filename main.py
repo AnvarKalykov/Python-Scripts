6c9c0c11b9a2891b2c01c0eea796c61b4f1edabb
Anvar Kalykov <kalykov.anvar@gmail.com>
2021-10-18 15:48:49 +0600

    18.10 string replace

--- main.py
+++ main.py
@@ -6,19 +6,10 @@ PATHTOBEOBSERVED = 'C:/Users/Anvar Kalykov/Desktop/check_file'
 
 
 def on_created(event):
-    print(f"hey, {event.src_path} has been created!")
+    s = event.src_path
+    head, sep, tail = s.replace('C:/Users/Anvar Kalykov/Desktop/check_file\\', '').partition('_')
+    print(head)
 
-
-def on_deleted(event):
-    print(f"what the f**k! Someone deleted {event.src_path}!")
-
-
-def on_modified(event):
-    print(f"hey buddy, {event.src_path} has been modified")
-
-
-def on_moved(event):
-    print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
 
 
 if __name__ == "__main__":
@@ -31,8 +22,7 @@ if __name__ == "__main__":
 
     my_event_handler.on_created = on_created
     my_event_handler.on_deleted = on_deleted
-    my_event_handler.on_modified = on_modified
-    my_event_handler.on_moved = on_moved
+
 
     path = PATHTOBEOBSERVED
     go_recursively = True
