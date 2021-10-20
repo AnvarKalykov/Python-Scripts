ef8dbc216f12143871b2cf451b6b538a1956ad00
Anvar Kalykov <kalykov.anvar@gmail.com>
2021-10-20 15:03:30 +0600

    18.10 connect to DB (don't work)

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import time
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
+import psycopg2
+from psycopg2 import Error
+
+
 PATHTOBEOBSERVED = 'C:/Users/Anvar Kalykov/Desktop/check_file'
 
 
@@ -9,6 +13,20 @@ def on_created(event):
     s = event.src_path
     head, sep, tail = s.replace('C:/Users/Anvar Kalykov/Desktop/check_file\\', '').partition('_')
     print(head)
+
+    try:
+        connection = psycopg2.connect(user="postgres", password=12345678, host="127.0.0.1", port=5432, database="postgres")
+        cursor = connection.cursor()
+        cursor.execute = "SELECT name, surename FROM mobile WHERE number = " + head + ";"
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row[0], row[1])
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+        connection.close()
+
+
 
 
 
@@ -21,7 +39,6 @@ if __name__ == "__main__":
     my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
 
     my_event_handler.on_created = on_created
-    my_event_handler.on_deleted = on_deleted
 
 
     path = PATHTOBEOBSERVED
@@ -33,7 +50,7 @@ if __name__ == "__main__":
     my_observer.start()
     try:
         while True:
-            time.sleep(0.3)
+            time.sleep(1)
     except KeyboardInterrupt:
         my_observer.stop()
     my_observer.join()
